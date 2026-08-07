af73e6aa129608f39f552255eb56bec0e369b35a
Karthik Mallina <cloudbykarthikmallina@gmail.com>
2026-08-07 18:32:31 +0530

    Terraform Roboshop EKS

---
+++ 70-acm/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "domain_name" {
+    default = "dawsc.fun"
+}
+
+variable "zone_id" {
+    default = "Z03275711LSDTWJWOX2S7"
+}
