636a8cccbb97c25c090c1c85b21bddb92cedae32
Karthik Mallina <cloudbykarthikmallina@gmail.com>
2026-08-07 21:30:44 +0530

    Terraform Roboshop EKS

---
+++ 80-frontend-alb/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.40.0"
+    }
+  }
+  backend "s3" {
+    bucket = "cmk-remote-state-dev-bucket-12" 
+    key = "roboshop-eks-frontend-alb-rules"
+    region = "us-east-1"
+    use_lockfile = true
+    encrypt = true
+  }
+}
+
+# Region  for Provider and Infra
+provider "aws" {
+  region = "us-east-1"
+}
---
+++ 80-frontend-alb/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z03275711LSDTWJWOX2S7"
+}
+
+variable "domain_name" {
+    default = "dawsc.fun"
+}
